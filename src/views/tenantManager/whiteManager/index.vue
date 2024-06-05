c0bacfb7468cf7fd669415ec62507883e516bc8a
kentaurse <kentaurse0212@gmail.com>
2024-06-05 17:12:04 +0900

    白名单搜索类型判断

--- src/views/tenantManager/whiteManager/index.vue
+++ src/views/tenantManager/whiteManager/index.vue
@@ -21,10 +21,7 @@
           :columns="dynamicColumns"
           :pagination="{ ...pagination, pageSizes: [5, 10, 20, 50, 100] }"
           :paginationSmall="size === 'small' ? true : false"
-          :header-cell-style="{
-            background: 'var(--el-fill-color-light)',
-            color: 'var(--el-text-color-primary)'
-          }"
+          :header-cell-style="tableHeaderStyle"
           @selection-change="handleSelectionChange"
           @page-size-change="handleTableWidthChange"
           @page-current-change="handleCurrentChange"
@@ -77,8 +74,6 @@
 
 <script setup lang="ts">
 import { PureTableBar } from '@/components/RePureTableBar';
-// import Delete from '@iconify-icons/ep/delete';
-// import AddFill from '@iconify-icons/ri/add-circle-line';
 import { useWhiteHook } from './utils/hook';
 import SearchForm from './component/SearchForm.vue';
 import { columns } from './component/TableColumnList';
@@ -86,6 +81,7 @@ import { t } from '@/plugins/i18n';
 import { usePublicHooks } from '@/hooks';
 
 const { switchStyle } = usePublicHooks();
+const { tableHeaderStyle } = usePublicHooks();
 defineOptions({ name: 'TENANTMANAGER_WHITEMANAGER' });
 
 const {
